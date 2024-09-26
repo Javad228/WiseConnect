6416b03b3507a55e2858a97435b50a86531ec996
Javad228 <jbaghiro@purdue.edu>
2024-09-26 00:59:11 -0400

    Added home page with dummy data

---
+++ wise_connect/android/app/src/main/kotlin/com/example/wise_connect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wise_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ wise_connect/lib/main.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
+
+void main() {
+  runApp(MyApp());
+  WakelockPlus.enable();
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(), // Using Google Fonts
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi User',
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Find your specialist',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search specialist...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Top Specialist in your Area',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SpecialistCard(
+                    name: 'Alexa Brown',
+                    specialization: 'Lashes & Brows',
+                    industry: 'Beauty Salon',
+                    distance: '1.27km',
+                    workingHours: '10:00 AM - 22:00 PM',
+                    imageUrl: 'assets/images/alexabrown.jpg',
+                  ),
+                  SpecialistCard(
+                    name: 'Jonathan Wayne',
+                    specialization: 'Tattoo',
+                    industry: 'Tattoo art',
+                    distance: '2.7km',
+                    workingHours: '10:00 AM - 22:00 PM',
+                    imageUrl: 'assets/images/jonathanwayne.jpg',
+                  ),
+                  // Add more SpecialistCards if needed
+                ],
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Services',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                children: [
+                  ServiceIcon(label: 'Haircut', iconPath: 'assets/icons/haircut.png'),
+                  ServiceIcon(label: 'Bike Repair', iconPath: 'assets/icons/bikerepair.png'),
+                  ServiceIcon(label: 'Gym', iconPath: 'assets/icons/gym.png'),
+                  ServiceIcon(label: 'Nails', iconPath: 'assets/icons/nails.png'),
+                  ServiceIcon(label: 'Makeup', iconPath: 'assets/icons/makeup.png'),
+
+                  // Add more services as needed
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SpecialistCard extends StatelessWidget {
+  final String name;
+  final String specialization;
+  final String distance;
+  final String workingHours;
+  final String imageUrl;
+  final String industry;
+
+  SpecialistCard({
+    required this.name,
+    required this.specialization,
+    required this.distance,
+    required this.workingHours,
+    required this.imageUrl,
+    required this.industry
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 270,
+      margin: EdgeInsets.only(right: 16),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(imageUrl),
+                    radius: 40,
+                  ),
+                  SizedBox(height: 6),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // Center the icon and text horizontally
+                    children: [
+                      Icon(
+                        Icons.location_pin, // Use the location pin icon
+                        color: Colors.blue, // Set the icon color to match your design
+                        size: 16, // Adjust the size as needed
+                      ),
+                      SizedBox(width: 4), // Add some spacing between the icon and the text
+                      Text(
+                        distance,
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(width: 30),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      specialization,
+                    ),
+                    Text(
+                      industry,
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                    Text(
+                      workingHours,
+                      style: TextStyle(color: Colors.green, fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class ServiceIcon extends StatelessWidget {
+  final String label;
+  final String iconPath;
+
+  ServiceIcon({required this.label, required this.iconPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(iconPath, height: 50, width: 50),
+        SizedBox(height: 4),
+        Text(label, style: TextStyle(fontSize: 14)),
+      ],
+    );
+  }
+}
